5453cc75411b825e2b7de86df4db2b87bc7c6226
sridhar-5 <kv.sridharsai@gmail.com>
2021-07-18 23:19:14 +0530

    addedsome comments

--- optimize-snake/AvoidHittingSelfBody.js
+++ optimize-snake/AvoidHittingSelfBody.js
@@ -12,6 +12,9 @@ function AvoidHittingSelfBody(nextMove, bodyObject) {
 }
 
 function CheckSnakeHeadNotInBody(GetCoordinates, SnakeBodyExceptTail) {
+  //if the head coordinates of the snake is somewhere inside the same snakes
+  //body then that might be considered as a self collision position
+  //head is also the [part] of snakes body but head in snakes body is not a safe place to move to.
   SnakeBodyExceptTail.forEach((bodyCoordinate) => {
     if (JSON.stringify(bodyCoordinate) == JSON.stringify(GetCoordinates)) {
       return false;
